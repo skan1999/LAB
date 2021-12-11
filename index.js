27c4233fa3b0773695fb7127c4ada654761145dd
skan1999 <skan572015@gmail.com>
2021-12-11 01:58:15 +0100

    last commit

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
+const testRouter = require('./route')
 
 const server = express()
 const port = process.env.PORT || 8000
 
 server.use(cors())
 server.use(express.json())
+server.use('/test', testRouter)
 
 server.get('/', (req, res) => {
     res.send('<h1>This is a test application</h1>')
@@ -15,4 +17,5 @@ server.get('/', (req, res) => {
 
 server.listen(port, () => {
     console.log(`\n=== Server listening on port ${port} ===\n`)
+
 })
